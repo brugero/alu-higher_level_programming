cbe9f253f9b4e3f1937d92124bbfb5e410c926b1
brugero <f.rugero@alustudent.com>
2025-02-25 09:30:56 -0800

    if_else_loops_functions

--- python-if_else_loops_functions/4-print_hexa.py
+++ python-if_else_loops_functions/4-print_hexa.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 for i in range(99):
-    print(f"{i} = {hex(i)}")
+    print("{} = {}".format(i, hex(i)), end=", ")
+print("98 = {}".format(hex(98)))
--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def uppercase(str):
-    """Print a string in uppercase followed by a new line."""
+    """Prints a string in uppercase followed by a new line."""
     result = ""
-    for c in str:
-        if ord('a') <= ord(c) <= ord('z'):
-            # Convert lowercase to uppercase by subtracting 32 from ASCII value
-            result += chr(ord(c) - 32)
+    for char in str:
+        if 'a' <= char <= 'z':
+            uppercase_char = chr(ord(char) - ord('a') + ord('A'))
         else:
-            result += c
-    print(result)
+            uppercase_char = char
+        result += uppercase_char
+    print("{}".format(result))
