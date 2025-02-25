ae0cce437ef31b389feaec055b49ee01aa5a619c
brugero <f.rugero@alustudent.com>
2025-02-25 10:55:48 -0800

    import modules

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -5,11 +5,11 @@ if __name__ == "__main__":
     num_args = len(argv) - 1
 
     if num_args == 0:
-        print("Number of argument(s): 0.")
+        print("argument(s): 0.")
     elif num_args == 1:
-        print("Number of argument(s): 1 argument:")
+        print("1 argument:")
         print("1: {}".format(argv[1]))
     else:
-        print("Number of argument(s): {} arguments:".format(num_args))
+        print("argument(s): {} arguments:".format(num_args))
         for i in range(1, len(argv)):
             print("{}: {}".format(i, argv[i]))
